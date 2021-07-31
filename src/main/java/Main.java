11c67d823739695f5e1d493e3969778b33e15284
pwan365 <pwan365@aucklanduni.ac.nz>
2021-07-31 14:55:35 +1200

    Added placeholder input options in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,24 +3,36 @@ import algo.Task;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 import algo.ValidScheduler;
 
-import java.util.Scanner;
-
 public class Main  {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        List commands = Arrays.asList(args);
+
+        String fileName = args[0];
+        int numberOfProcessors = Integer.parseInt(args[1]);
+
+        if (commands.contains("-p")){
+            //Parallel option
+        }
+
+        if (commands.contains("-v")){
+            //Visualize option
+        }
+
+        if (commands.contains("-o")){
+            //Output option
+        }
+
+        /*Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the processor number: ");
         int numberOfProcessors = scanner.nextInt();
         while (numberOfProcessors > 6 || numberOfProcessors <= 0 ) {
             System.out.print("Invalid processor number! Please enter a new number: ");
             numberOfProcessors = scanner.nextInt();
-        }
+        } */
 
         Processor[] processorPool  = new Processor[numberOfProcessors];
 
