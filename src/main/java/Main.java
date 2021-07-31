50a348beacd55a7e944bb9b1266b78ef0f7b1525
John Jia <chenhao0401@hotmail.com>
2021-07-31 14:17:09 +1200

    Topological Ordering and queue data structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,17 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
 import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
+import algo.ValidScheduler;
 
 public class Main  {
     public static void main(String[] args) {
-
+        ValidScheduler v = new ValidScheduler();
+        Queue<Node> nodeQueue = v.topologicalorder();
+        for (Node n : nodeQueue){
+            System.out.println(n.getId());
+        }
     }
 }
--- src/main/java/algo/ValidScheduler.java
+++ src/main/java/algo/ValidScheduler.java
@@ -1,4 +1,34 @@
 package algo;
 
+import io.InputReader;
+import org.graphstream.algorithm.TopologicalSortKahn;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class ValidScheduler {
+
+    public Queue<Node> topologicalorder(){
+
+        System.setProperty("org.graphstream.ui", "swing");
+
+
+        InputReader reader = new InputReader("input.dot");
+        Graph g = reader.read();
+        g.display();
+        TopologicalSortKahn tp = new TopologicalSortKahn();
+        tp.init(g);
+        tp.compute();
+        List<Node> nodeList = tp.getSortedNodes();
+
+        Queue<Node> nodeQueue = new LinkedList<>(nodeList);
+        return nodeQueue;
+
+    }
+
+
+
 }
