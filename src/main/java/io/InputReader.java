5468b96b2c00c50297a00d4a6290b2e1ddc9cd2b
ljey973 <69109895+ljey973@users.noreply.github.com>
2021-07-31 13:28:54 +1200

    Setting up framework

---
+++ src/main/java/io/InputReader.java
@@ -0,0 +1,27 @@
+package io;
+import java.io.IOException;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+import org.graphstream.stream.file.FileSource;
+import org.graphstream.stream.file.FileSourceDOT;
+public class InputReader {
+
+    private String input_file;
+    public InputReader(String input_file){
+        this.input_file = input_file;
+    }
+    public Graph read() {
+        Graph g = new DefaultGraph("Tut");
+        FileSource fs = new FileSourceDOT();
+        String dir = System.getProperty("user.dir");
+        fs.addSink(g);
+        try {
+            fs.readAll(dir + "\\" + input_file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            fs.removeSink(g);
+        }
+        return g;
+    }
+}
