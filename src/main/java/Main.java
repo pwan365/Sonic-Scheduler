b91853e7221e14ce2cad46216f23c4307fffc6ac
myao209 <myao209@aucklanduni.ac.nz>
2021-07-31 14:20:43 +1200

    Add user input read for processor pool

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,22 @@
+import algo.Processor;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Queue;
-
-import algo.ValidScheduler;
+import java.util.Scanner;
 
 public class Main  {
     public static void main(String[] args) {
-        ValidScheduler v = new ValidScheduler();
-        Queue<Node> nodeQueue = v.topologicalorder();
-        for (Node n : nodeQueue){
-            System.out.println(n.getId());
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the processor number: ");
+        int numberOfProcessors = scanner.nextInt();
+        while (numberOfProcessors > 6 || numberOfProcessors <= 0 ) {
+            System.out.print("Invalid processor number! Please enter a new number: ");
+            numberOfProcessors = scanner.nextInt();
         }
+
+        Processor[] processorPool  = new Processor[numberOfProcessors];
     }
 }
