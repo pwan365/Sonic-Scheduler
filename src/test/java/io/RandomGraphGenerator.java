0943b4def26bb9cebbdd34e2d324972614f6cbff
myao209 <myao209@aucklanduni.ac.nz>
2021-08-02 21:18:20 +1200

    Change all random bound to static fields and make sure none of them will be 0

--- src/test/java/io/RandomGraphGenerator.java
+++ src/test/java/io/RandomGraphGenerator.java
@@ -11,16 +11,22 @@ import java.io.IOException;
 import java.util.Random;
 
 public class RandomGraphGenerator {
+
+    private static int MAX_NUM_OF_TASKS = 10;
+    private static int MAX_NODE_WEIGHT = 10;
+    private static int MAX_EDGE_WEIGHT = 10;
+
+
     public static void main(String[] args) {
         Random randomProducer = new Random();
 
-        int numOfTasks = randomProducer.nextInt(10);
-        String outputName = "textingGraph";
+        int numOfTasks = randomProducer.nextInt(MAX_NUM_OF_TASKS);
+        String outputName = "textingGraph.dot";
         Graph dotGraph = new DefaultGraph(outputName);
 
         for (int i = 0; i < numOfTasks; i++){
             Node node = dotGraph.addNode(String.valueOf(i));
-            node.setAttribute("Weight",randomProducer.nextInt(10));
+            node.setAttribute("Weight",randomProducer.nextInt(MAX_NODE_WEIGHT) + 1);
         }
 
         for (int i = 0; i < numOfTasks; i++){
@@ -28,7 +34,7 @@ public class RandomGraphGenerator {
                 if(randomProducer.nextBoolean()) {
                     String label = i + "-" + j;
                     Edge edge = dotGraph.addEdge(label, i, j, true);
-                    edge.setAttribute("Weight", randomProducer.nextInt(10) + 1);
+                    edge.setAttribute("Weight", randomProducer.nextInt(MAX_EDGE_WEIGHT) + 1);
                 }
             }
         }
