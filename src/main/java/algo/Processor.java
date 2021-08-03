2c11819389236515fa874ba9f9a95ccee74713d7
pwan365 <pwan365@aucklanduni.ac.nz>
2021-08-03 17:49:08 +1200

    Refactoring and commenting on Processor

--- src/main/java/algo/Processor.java
+++ src/main/java/algo/Processor.java
@@ -3,37 +3,70 @@ package algo;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-
+/**
+ * The Processor class is used to represent a processor that can run Tasks.
+ *
+ * @author
+ */
 public class Processor {
-    private Double latest_time = 0.0;
-    private HashMap<Task, ArrayList<Double>> node_table = new HashMap<>();
+    //The time when the processor is free.
+    private Double latestTime = 0.0;
+    //A map to store the nodes running on this process and their information.
+    private HashMap<Task, ArrayList<Double>> nodeTable = new HashMap<>();
+    //The id of the processor.
     private int processNum;
 
+    /**
+     * The constructor of Processor.
+     * @param processNumber The given id to this processor.
+     */
     public Processor(int processNumber) {
         this.processNum = processNumber;
     }
 
+    /**
+     * This method returns the latest time this processor is free.
+     * @return The latest time this processor is free.
+     */
     public Double getLatestTime() {
-        return latest_time;
+        return latestTime;
     }
 
-    public void setLatestTime(Double latest_time) {
-        this.latest_time = latest_time;
+    /**
+     * This method sets the latest time this processor is free.
+     * @param latestTime The new latest time.
+     */
+    public void setLatestTime(Double latestTime) {
+        this.latestTime = latestTime;
     }
 
-
+    /**
+     * This method is used to add a task to this processor.
+     * @param task The task object to be added to the processor.
+     * @param start The start time of the task.
+     * @param end The end time of the task.
+     * @param duration The duration of the task.
+     */
     public void addTask(Task task, Double start, Double end, Double duration){
         ArrayList<Double> task_info = new ArrayList<>();
         task_info.add(start);
         task_info.add(end);
         task_info.add(duration);
-        this.node_table.put(task,task_info);
+        this.nodeTable.put(task,task_info);
     }
 
+    /**
+     * This method returns all the tasks that run on this processor and their information.
+     * @return A hashmap containing the task and their timings.
+     */
     public HashMap<Task, ArrayList<Double>> getTasks() {
-        return node_table;
+        return nodeTable;
     }
 
+    /**
+     * This method returns the id of this processor.
+     * @return The id of this processor.
+     */
     public int getProcessNum() {
         return processNum;
     }
