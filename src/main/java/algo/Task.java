1d7c06d9362fd20f71cbd20ae452bfb104dac258
pwan365 <pwan365@aucklanduni.ac.nz>
2021-08-03 18:30:16 +1200

    Refactoring and commenting on Task

--- src/main/java/algo/Task.java
+++ src/main/java/algo/Task.java
@@ -5,14 +5,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * This class is used to record a Node from the graph and its relative information.
+ *
+ * @author
+ */
 public class Task {
     private double finishingTime = -1.0;
     private double startingTime;
     private double durationTime;
+    //This is the list of edges that connects to the parent of the node of this task.
     private ArrayList<Edge> parentEdgeList = new ArrayList<Edge>();
+    //The node that represents this task in a graph.
     private Node node;
+    //The processor this task is allocated to.
     private Processor allocatedProcessor;
 
+    /**
+     * The constructor of Task.
+     * @param node The node to be referenced with this task.
+     */
     public Task(Node node){
         this.node = node;
         this.durationTime = (Double)node.getAttribute("Weight");
@@ -22,40 +34,70 @@ public class Task {
         }
     }
 
+    /**
+     * This method returns the finishing time for a task on the assigned processor. Returns -1.0 if not set.
+     * @return Task finishing time.
+     */
     public double getFinishingTime() {
         return finishingTime;
     }
 
+    /**
+     * This method return the node that represents the task in a graph.
+     * @return Node associated with this task.
+     */
     public Node getNode() {
         return node;
     }
 
-
+    /**
+     * This method sets the node that represents the task in a graph.
+     * @param node The node to be associated with this task.
+     */
     public void setNode(Node node) {
         this.node = node;
     }
 
-    // Parent nodes setter and getting
+    /**
+     * Returns the arraylist of edges going into the referenced node of this task.
+     * @return Arraylist of edges from parents.
+     */
     public ArrayList<Edge> getParentEdgeList() {
         return parentEdgeList;
     }
 
-    // Processor getter and setter
+    /**
+     * Getter for the processor this task is allocated to.
+     * @return The allocated processor.
+     */
     public Processor getAllocatedProcessor() {
         return allocatedProcessor;
     }
 
+    /**
+     * This method is used to set the data of this task when it is scheduled to a processor.
+     * @param allocated_processor The processor this task is scheduled in.
+     * @param finishing_time The finished time of this task on the scheduled processor.
+     * @param starting_time The start time of this task on the scheduled processor.
+     */
     public void setTaskDetails(Processor allocated_processor,Double finishing_time,Double starting_time) {
         this.allocatedProcessor = allocated_processor;
         this.finishingTime = finishing_time;
         this.startingTime = starting_time;
     }
 
-    // Starting time getter and setter
+    /**
+     * The getter for the starting time of this task.
+     * @return Starting time of this task.
+     */
     public double getStartingTime() {
         return startingTime;
     }
 
+    /**
+     * The getter for the duration of this task.
+     * @return Duration of this task.
+     */
     public double getDurationTime() {
         return durationTime;
     }
