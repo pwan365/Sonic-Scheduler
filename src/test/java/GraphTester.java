18c2deb29e1840c5dfe422c3ea2e10dcb0672333
Samuel Chen <68937749+Sparye@users.noreply.github.com>
2021-08-04 19:23:46 +1200

    added a test case input.dot/outputtry.dot

--- src/test/java/GraphTester.java
+++ src/test/java/GraphTester.java
@@ -5,6 +5,7 @@ import com.sun.org.apache.xpath.internal.objects.XString;
 import io.InputReader;
 import io.OutputWriter;
 import io.Validator;
+import jdk.internal.util.xml.impl.Input;
 import org.graphstream.graph.Graph;
 
 import org.junit.jupiter.api.Test;
@@ -89,6 +90,24 @@ public class GraphTester {
         Graph inputGraph = reader.read();
         String outputGraphFilename = scheduleHelper(inputGraph, 2);
         InputReaderHelper outputReader = new InputReaderHelper(outputGraphFilename);
+
+        Graph outputGraph = outputReader.read();
+        Validator validator = new Validator();
+        validator.initialize(inputGraph, outputGraph, 2);
+        assertTrue(validator.validate());
+    }
+
+    @Test
+    public void testInputDot() {
+//        String inputGraphPath = pathGetter("Nodes_11_OutTree.dot");
+//        InputReaderHelper reader = new InputReaderHelper(inputGraphPath);
+        System.out.println("new test start here");
+        InputReader reader = new InputReader("input.dot");
+
+        Graph inputGraph = reader.read();
+//        String outputGraphFilename = scheduleHelper(inputGraph, 2);
+//        InputReaderHelper outputReader = new InputReaderHelper(outputGraphFilename);
+        InputReader outputReader = new InputReader("outputtry.dot");
 
         Graph outputGraph = outputReader.read();
         Validator validator = new Validator();
