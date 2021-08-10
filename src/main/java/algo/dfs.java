c5693a3b522ebe42692642fae8db7d100608bdf0
myao209 <myao209@aucklanduni.ac.nz>
2021-08-10 20:11:10 +1200

    Finish implementing basic valid order method

--- src/main/java/algo/dfs.java
+++ src/main/java/algo/dfs.java
@@ -1,6 +1,11 @@
 package algo;
 
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.Node;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class dfs {
 
@@ -17,8 +22,39 @@ public class dfs {
      * TODO should memoize the solution into a hashmap so there are no repeated calculations.
      */
     public ArrayList<Task> validOrder(ArrayList<Task> scheduledTasks) {
-        return null;
+        // hashMap and return list
+        Map<Node, Boolean> map = new HashMap<>();
+        ArrayList<Task> result = new ArrayList<>();
+
+        for(Task task : scheduledTasks){
+            Node node = task.getNode(); // scheduled task node
+            if(map.get(node)){
+                result.add(task); // if node is already in valid map, then we don't need to look into it
+                continue;
+            }
+            ArrayList<Edge> parentEdges = task.getParentEdgeList();
+            boolean flag = false;
+            for(Edge e: parentEdges){
+                Node parentNode = e.getNode0();
+                for(Task scheduledTask : scheduledTasks) { // scan through all tasks to see whether the parent node exists
+                    if(scheduledTask.getNode() == parentNode){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    break;
+                }
+            }
+            if(flag){
+                map.put(node, true);
+                result.add(task);
+            }
+        }
+
+        return result;
     }
+
     public void branchBound(Task task, int processor) {
         /**
          * TODO Schedule task here.
