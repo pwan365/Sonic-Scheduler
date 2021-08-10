6f4bb8796a67eefd47c777a59e55579a2c8ec1af
myao209 <myao209@aucklanduni.ac.nz>
2021-08-10 20:51:32 +1200

    Update valid order method

--- src/main/java/algo/dfs.java
+++ src/main/java/algo/dfs.java
@@ -1,17 +1,21 @@
 package algo;
 
 import org.graphstream.graph.Edge;
+import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class dfs {
 
     private Schedule schedule;
     private Schedule bestSchedule;
     private int numProcessors;
+    private Graph graph;
     public dfs(int processors) {
         schedule = new Schedule(processors);
         schedule.setLatestScheduleTime(Integer.MAX_VALUE);
@@ -22,32 +26,38 @@ public class dfs {
      * TODO should memoize the solution into a hashmap so there are no repeated calculations.
      */
     public ArrayList<Task> validOrder(ArrayList<Task> scheduledTasks) {
-        // hashMap and return list
-        Map<Node, Boolean> map = new HashMap<>();
+        // hashMap and return list, and graph
+        Map<Task, Boolean> map = new HashMap<>();
         ArrayList<Task> result = new ArrayList<>();
+        int numberOfTasks = graph.getNodeCount();
 
-        for(Task task : scheduledTasks){
-            Node node = task.getNode(); // scheduled task node
-            if(map.get(node)){
-                result.add(task); // if node is already in valid map, then we don't need to look into it
-                continue;
-            }
-            ArrayList<Edge> parentEdges = task.getParentEdgeList();
-            boolean flag = false;
-            for(Edge e: parentEdges){
-                Node parentNode = e.getNode0();
-                for(Task scheduledTask : scheduledTasks) { // scan through all tasks to see whether the parent node exists
-                    if(scheduledTask.getNode() == parentNode){
-                        flag = true;
-                        break;
-                    }
+        // if input is 0 size, find all root tasks
+        if(scheduledTasks.size() == 0){
+            for(int i = 0; i < numberOfTasks; i++){
+                Node node = graph.getNode(i);
+                List<Edge> edges = node.enteringEdges().collect(Collectors.toList());
+                if(edges.size() == 0){
+                    Task nodeTask = (Task)node.getAttribute("Task");
+                    result.add(nodeTask);
                 }
-                if(!flag){
+            }
+            return result;
+        }
+
+        for(int i = 0; i < graph.getNodeCount(); i++){
+            Node node = graph.getNode(i);
+            Task task = (Task)node.getAttribute("Task");
+            List<Edge> parentEdges = task.getParentEdgeList();
+            boolean flag = true;
+            for(Edge edge: parentEdges){
+                Node parent = edge.getNode0();
+                Task parentTask = (Task)parent.getAttribute("Task");
+                if(!scheduledTasks.contains(parentTask)){
+                    flag = false;
                     break;
                 }
             }
-            if(flag){
-                map.put(node, true);
+            if(flag && !scheduledTasks.contains(task)){
                 result.add(task);
             }
         }
