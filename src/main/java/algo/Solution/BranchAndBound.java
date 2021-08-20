6e40039174126be93bbaef2de34ed5c43e73ae0d
ljey973 <69109895+ljey973@users.noreply.github.com>
2021-08-20 14:38:10 +1200

    started BranchAndBound class.

--- src/main/java/algo/Solution/BranchAndBound.java
+++ src/main/java/algo/Solution/BranchAndBound.java
@@ -1,7 +1,23 @@
 package algo.Solution;
 
+import java.util.HashSet;
+import java.util.Stack;
+
 public abstract class BranchAndBound {
     private int numTasks;
+
+    //Schedule information
+    private Stack<Integer> time;
+    private Integer idle;
+    private HashSet<Integer> scheduledTasks;
+    private HashSet<Integer> unscheduledTasks;
+
+    //Processor information
+    private int [] processorTimes;
+
+    //Task information
+    private int [] taskProcessors;
+
     public BranchAndBound(int tasks) {
         numTasks = tasks;
     }
