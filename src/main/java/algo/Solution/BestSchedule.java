cd3adb29d2b82b5849be42e13c0a00ffd645bc77
myao209 <myao209@aucklanduni.ac.nz>
2021-08-22 20:25:02 +1200

    Refined comment for BestSchedule class

--- src/main/java/algo/Solution/BestSchedule.java
+++ src/main/java/algo/Solution/BestSchedule.java
@@ -10,21 +10,26 @@ import org.graphstream.graph.Node;
  * @author Wayne Yao, Luxman Jeyarajah
  */
 public class BestSchedule {
+    private int[][] taskInformation; // copy of task information
+    private int[] taskProcessors; // copy of allocated processor list
+    protected int bestTime; // best time
 
-
-    private int[][] taskInformation;
-    private int[] taskProcessors;
-    protected int bestTime;
-
-
+    /**
+     * Constructor for the class, set best time to MAX integer value so that it can be compared
+     */
     public BestSchedule(){
         bestTime = Integer.MAX_VALUE;
     }
 
+    /**
+     * The main method of this class, it will make copies of the task info, task processor and the best time
+     * @param candidateBest best time
+     * @param allocatedProcessors task allocation list
+     * @param taskData task information list
+     */
     public void makeCopy(int candidateBest, int[] allocatedProcessors, int[][] taskData) {
         taskInformation = new int[taskData.length][4];
         taskProcessors = new int[allocatedProcessors.length];
-
 
         for(int i = 0; i < allocatedProcessors.length; i++){
             taskProcessors[i] = allocatedProcessors[i];
@@ -37,19 +42,35 @@ public class BestSchedule {
         this.bestTime = candidateBest;
     }
 
+    /**
+     * Getter for task information array
+     * @return an array storing task information
+     */
     public int[][] getTaskInformation() {
         return taskInformation;
     }
 
-
-
+    /**
+     * Getter for task allocation processor list
+     * @return an array storing allocated processor for each task
+     */
     public int[] getTaskProcessors() {
         return taskProcessors;
     }
+
+    /**
+     * Getter for best time
+     * @return best time
+     */
     public int getTime() {
         return bestTime;
     }
 
+    /**
+     * Will be called after everything is finished
+     * Start time and allocated processor will be added to the graph nodes
+     * @param graph the input graph object
+     */
     public void writeToGraph(Graph graph){
         int nodeCount = graph.getNodeCount();
         for (int i = 0; i < nodeCount; i++){
